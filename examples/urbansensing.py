e061173fa20e7b699c342a30b01c318de4f15923
Alexander Lesjak <e01427747@student.tuwien.ac.at>
2022-09-25 23:27:54 +0200

    creates initial example to play with

--- examples/urbansensing.py
+++ examples/urbansensing.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import simpy
 from srds import ParameterizedDistribution
 
 import ether.blocks.nodes as nodes
 from ether.blocks.cells import IoTComputeBox, Cloudlet, FiberToExchange, MobileConnection
 from ether.cell import SharedLinkCell, GeoCell
-from ether.core import Node, Link
+from ether.core import Node, Link, Flow
 from ether.topology import Topology
 from ether.vis import draw_basic
 
@@ -20,7 +21,16 @@ def node_name(obj):
         return str(obj)
 
 
-def main():
+def counter():
+    i = 0
+    st = "halo"
+    while True:
+        yield from (i,st) # ist ein blocking call
+        i += 1
+
+
+
+def main5():
     topology = Topology()
 
     aot_node = IoTComputeBox(nodes=[nodes.rpi3, nodes.rpi3])
@@ -47,6 +57,78 @@ def main():
     plt.show()  # display
 
     print('num nodes:', len(topology.nodes))
+
+
+def main2():
+    print("Hi")
+    gen = counter()
+    print(next(gen))
+    print(next(gen))
+    print(next(gen))
+    return gen
+
+
+def main3(gen):
+    print(next(gen))
+    print(next(gen))
+    print(next(gen))
+
+
+
+def printer(env):
+    while True:
+        print(env.now)
+        yield env.timeout(1)
+
+
+def wrapper(source, dest, size, env, topology, results):
+    route = topology.route(source, dest)
+    flow1 = Flow(env, size, route)
+    start_ts = env.now
+    yield flow1.start()
+    end_ts = env.now
+    results.append(end_ts - start_ts)
+
+
+
+
+
+def create_topology():
+    topology = Topology()
+
+    aot_node = IoTComputeBox(nodes=[nodes.rpi3, nodes.rpi3])
+    neighborhood = lambda size: SharedLinkCell(
+        nodes=[
+            [aot_node] * size,
+            IoTComputeBox([nodes.nuc] + ([nodes.tx2] * size * 2))
+        ],
+        shared_bandwidth=500,
+        backhaul=MobileConnection('internet_chix'))
+    city = GeoCell(
+        5, nodes=[neighborhood], density=lognorm((0.82, 2.02)))
+    cloudlet = Cloudlet(
+        5, 2, backhaul=FiberToExchange('internet_chix'))
+
+    topology.add(city)
+    topology.add(cloudlet)
+    return topology
+
+
+
+def main():
+    topology = create_topology()
+    env = simpy.Environment()
+    source = topology.get_nodes()[0]
+    dest = topology.get_nodes()[1]
+    size = 10000000
+    results = []
+    env.process(wrapper(source, dest, size, env, topology, results))
+    env.process(wrapper(topology.get_nodes()[3], topology.get_nodes()[4], size, env, topology, results))
+    env.process(wrapper(topology.get_nodes()[3], topology.get_nodes()[4], size, env, topology, results))
+    env.run(until=3)
+    print(results)
+
+
 
 
 if __name__ == '__main__':
